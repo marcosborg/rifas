e93bc16d3bd7ee1f8412f1724af0644a2db3d715
marcosborg <m.borges.mail@gmail.com>
2024-05-24 14:08:27 +0100

    Update

--- app/Http/Controllers/MarketplaceController.php
+++ app/Http/Controllers/MarketplaceController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ProductCategory;
+use App\Models\Company;
+use App\Models\Product;
 
 class MarketplaceController extends Controller
 {
     public function index()
     {
-        return view('website.marketplace.index');
+
+        $product_categories = ProductCategory::all();
+        $companies = Company::all();
+        $products = Product::all();
+
+        return view('website.marketplace.index', compact('product_categories', 'companies', 'products'));
     }
 }
--- resources/views/website/marketplace/index.blade.php
+++ resources/views/website/marketplace/index.blade.php
@@ -9,13 +9,67 @@
             </div>
         </div>
 
-        <div class="row mb-5">
+        <div class="row">
 
-            <img src="/assets/marketplace.jpg" alt="">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-header text-bg-success">
+                        Categorias de produtos
+                    </div>
+                    <div class="list-group list-group-flush">
+                        @foreach ($product_categories as $product_category)
+                        <a class="list-group-item" href="#">{{ $product_category->name }}</a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-9">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            @foreach ($companies as $company)
+                            <div class="col-2">
+                                <div class="card">
+                                    <img src="{{ $company->logo->getUrl() }}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $company->name }}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <div class="row">
+                            @foreach ($products as $product)
+                            <div class="col-4">
+                                <div class="card">
+                                    <img src="{{ $product->photo->getUrl() }}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $product->name }}</h5>
+                                        <h1>{{ $product->price }} <span class="small">€</span></h1>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
 
     </div>
 </section>
 
+@endsection
+@section('styles')
+    <style>
+        span.small {
+            font-size: 80%;
+        }
+    </style>
 @endsection
