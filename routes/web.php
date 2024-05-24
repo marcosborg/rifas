687166a133a929a71b1acaa1a709477fd1d6b456
marcosborg <m.borges.mail@gmail.com>
2024-05-24 12:39:12 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -147,12 +147,6 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::post('products/ckmedia', 'ProductController@storeCKEditorImages')->name('products.storeCKEditorImages');
     Route::resource('products', 'ProductController');
 
-    // Company
-    Route::delete('companies/destroy', 'CompanyController@massDestroy')->name('companies.massDestroy');
-    Route::post('companies/media', 'CompanyController@storeMedia')->name('companies.storeMedia');
-    Route::post('companies/ckmedia', 'CompanyController@storeCKEditorImages')->name('companies.storeCKEditorImages');
-    Route::resource('companies', 'CompanyController');
-
     Route::get('system-calendar', 'SystemCalendarController@index')->name('systemCalendar');
 });
 Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
